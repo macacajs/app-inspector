2f145f0e99167e9d0864421f4764c91ee55a5c28
xudafeng <xudafeng@126.com>
2016-09-26 08:37:32 +0800

    add unittest

--- test/app-inspector.test.js
+++ test/app-inspector.test.js
@@ -1,9 +1,26 @@
 'use strict';
 
-var Inspector = require('..');
+const path = require('path');
+const EOL = require('os').EOL;
+const CliTest = require('command-line-test');
 
-describe('test', function() {
-  it('should be ok', function() {
-    Inspector.should.be.ok();
+const pkg = require('../package');
+
+const binFile = path.resolve(pkg.bin[pkg.name]);
+
+describe('command-line test', function() {
+
+  it('`app-inspector -v` should be ok', function *() {
+    var cliTest = new CliTest();
+    var res = yield cliTest.execFile(binFile, ['-v'], {});
+    res.stdout.should.containEql(pkg.version);
   });
+
+  it('`app-inspector -h` should be ok', function *() {
+    var cliTest = new CliTest();
+    var res = yield cliTest.execFile(binFile, ['-h'], {});
+    var lines = res.stdout.trim().split(EOL);
+    lines[0].should.containEql(pkg.name);
+  });
+
 });
