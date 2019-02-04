3d501fc188dc3ab3fa72df826649ef367588a552
达峰的夏天 <xudafeng@126.com>
2019-02-04 12:52:03 +0800

    feat: support ios real device (#218)

---
+++ test/common/helper.test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const assert = require('assert');
+
+const _ = require('../../lib/common/helper');
+
+describe('common/helper.test.js', () => {
+  let res;
+  it('getDeviceInfo()', () => {
+    res = _.getDeviceInfo('00008020-001D4D38XXXXXXXX');
+    assert.equal(res.isIOS, true);
+    assert.equal(res.isRealIOS, true);
+    res = _.getDeviceInfo('B8B6B1F5-30E0-401B-B1CE-70E4F39937A5');
+    assert.equal(res.isIOS, true);
+    assert.equal(res.isRealIOS, false);
+  });
+});
